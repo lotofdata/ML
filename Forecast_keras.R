bacbcbb9829ab1d0ff6419b547e0237924d4531f
Darius Simsonas <simsonas.darius@gmail.com>
2018-04-20 22:26:13 +0300

    initial commit

---
+++ Forecast_keras.R
@@ -0,0 +1,22 @@
+# Core Tidyverse
+library(tidyverse)
+library(glue)
+library(forcats)
+
+# Time Series
+library(timetk)
+library(tidyquant)
+library(tibbletime)
+
+# Visualization
+library(cowplot)
+
+# Preprocessing
+library(recipes)
+
+# Sampling / Accuracy
+library(rsample)
+library(yardstick) 
+
+# Modeling
+library(keras)
